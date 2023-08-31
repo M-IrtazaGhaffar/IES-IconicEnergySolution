a0260592d5c53841e19e128855791075495a3ac4
Muhammad Irtaza Ghaffar <93589223+M-IrtazaGhaffar@users.noreply.github.com>
2023-08-31 22:43:56 +0500

    11

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,37 +5,11 @@ import Logo from "../assets/Logo1.png";
 function About() {
     return (
         <div className='p-10 relative z-10 bgGen text-white'>
-            <div className="m-w-[90vw] absolute z-30 overflow-hidden">
-                <div
-                    className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
-                    aria-hidden="true"
-                >
-                    <div
-                        className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
-                        style={{
-                            clipPath:
-                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                        }}
-                    />
-                </div>
-                <div
-                    className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
-                    aria-hidden="true"
-                >
-                    <div
-                        className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
-                        style={{
-                            clipPath:
-                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                        }}
-                    />
-                </div>
-            </div>
             <h3 className='text-6xl font-extrabold text-center p-5'>Yes!</h3>
             <p className='text-center font-bold'>Iconic Energy Solution is renowned service provideer company dealing in Standby Power, Civil Works and Air Conditioning Systems in the Banking, Restaurants, Textile & Education sectors.</p>
             <div className='flex items-center lg:justify-evenly justify-between gap-10 flex-col md:flex-row-reverse lg:p-10'>
                 <Image src={Logo} width={300} />
-                <div className='w-[70%] lg:w-[50%]'>
+                <div className='lg:w-[50%]'>
                     <h3 className='py-5 underline underline-offset-8 text-4xl font-bold tracking-tight text-white sm:text-6xl"'>Why Us?</h3>
                     <p className='text-sm'>
                         Iconic Energy Solution is a world class service provider company dealing in Standby Power, Civil Works and Air Conditioníng Systems in the Banking, Restaurants, Textile & Education sectors. We have-managed to gain a foothold in the emerging but very competitive operations and maintenance industry of Pakistan.
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function Header() {
     return (
         <div className='min-h-screen flex flex-col justify-center items-center gap-6 text-center p-10'>
-            <h1 className='text-4xl lg:text-6xl font-bold flex flex-col gap-2'>
+            <h1 className='text-4xl lg:text-6xl font-bold flex flex-col gap-2 z-10'>
                 So! You're in Search of
                 <span className='text-indigo-500 text-6xl lg:text-8xl'>
                     Energy Solutions?
                 </span>
             </h1>
-            <p>
+            <p className='z-10'>
                 Then you&#39;re at the right place. No need to worry about it, we&#39;re here for you. <span className='font-semibold'>Iconic Energy Solutions</span> is one of best Energy problems solvers all over the <span className='text-teal-700 font-semibold'>Pakistan</span> & they&#39;ll help you to get rid of it.
             </p>
             <button className='bg-indigo-500 px-4 py-2 rounded text-white flex items-center gap-2 boxShadow'>
@@ -18,14 +18,14 @@ function Header() {
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-arrow-down mt-5 relative animate-bounce" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z" />
             </svg>
-            <div className='w-12 h-12 bg-sky-700 blur-2xl absolute top-[15%] left-[60%]'></div>
-            <div className='w-12 h-12 bg-sky-700 blur-2xl absolute top-[30%] left-[30%]'></div>
-            <div className='w-12 h-12 bg-sky-700 blur-2xl absolute top-[45%] left-[45%]'></div>
-            <div className='w-12 h-12 bg-sky-700 blur-2xl absolute top-[60%] left-[15%]'></div>
-            <div className='w-12 h-12 bg-sky-700 blur-2xl absolute top-[35%] left-[70%]'></div>
-            <div className='w-12 h-12 bg-sky-700 blur-2xl absolute top-[50%] left-[50%]'></div>
-            <div className='w-12 h-12 bg-sky-700 blur-2xl absolute top-[20%] left-[85%]'></div>
-            <div className='w-12 h-12 bg-sky-700 blur-2xl absolute top-[65%] left-[35%]'></div>
+            <div className='w-12 h-12 bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-90 blur-2xl absolute top-[15%] left-[60%]'></div>
+            <div className='w-12 h-12 bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-90 blur-2xl absolute top-[30%] left-[30%]'></div>
+            <div className='w-12 h-12 bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-90 blur-2xl absolute top-[45%] left-[45%]'></div>
+            <div className='w-12 h-12 bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-90 blur-2xl absolute top-[60%] left-[15%]'></div>
+            <div className='w-12 h-12 bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-90 blur-2xl absolute top-[35%] left-[70%]'></div>
+            <div className='w-12 h-12 bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-90 blur-2xl absolute top-[50%] left-[50%]'></div>
+            <div className='w-12 h-12 bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-90 blur-2xl absolute top-[20%] left-[85%]'></div>
+            <div className='w-12 h-12 bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-90 blur-2xl absolute top-[65%] left-[35%]'></div>
         </div>
     )
 }
