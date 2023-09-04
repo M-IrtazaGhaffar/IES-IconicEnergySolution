3cdc0192c697326d587aca2756b0057e938f9293
Muhammad Irtaza Ghaffar <93589223+M-IrtazaGhaffar@users.noreply.github.com>
2023-09-05 01:34:23 +0500

    111

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,29 +1,35 @@
+import Link from 'next/link'
 import React from 'react'
 // bg-[#576CBC]
 
 function Footer() {
     return (
-        <section className='bg-white shadow-2xl '>
+        <section className='bg-white shadow-2xl'>
             <footer className="footer p-10 bg-indigo-500 text-white rounded-t-3xl">
-                <div>
-                    <span className="footer-title">Services</span>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                <div className='w-[50vw]'>
+                    <h3 className='text-2xl font-bold'>
+                        Iconic Energy Solutions
+                    </h3>
+                    <p className='text-xs'>
+                        Iconic Energy Solution is renowned service provider company dealing in Standby Power, Civil Works and Air Conditioning Systems in the Banking, Restaurants, Textile & Education sectors.
+                    </p>
+                    <ul className='flex flex-col gap-2 py-5'>
+                        <li>Mail us at <a href="mailto:Info@iconicenergy.com.pk">Info@iconicenergy.com.pk</a>
+                        </li>
+                        <li>
+                            Call us on <a href="tel:+923335390253" className='text-sm'>0333-5390253</a>
+                        </li>
+                    </ul>
                 </div>
                 <div>
                     <span className="footer-title">Company</span>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
-                </div>
-                <div>
-                    <span className="footer-title">Legal</span>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+                    <Link className='link link-hover' href='/ac'>AC Inverter</Link>
+                    <Link className={`transition-all outline-none`} href='/ats'>ATS</Link>
+                    <Link className={`transition-all outline-none`} href='/cctv'>CCTV</Link>
+                    <Link className={`transition-all outline-none`} href='/others'>Others</Link>
+                    <Link className={`transition-all outline-none`} href='/contact'>Contact Us</Link>
+                    <Link className={`transition-all outline-none`} href='/load'>Load Calculator</Link>
+                    <Link className={`transition-all outline-none`} href='/applications'>Applications</Link>
                 </div>
             </footer>
             <footer className="footer footer-center p-4 bg-[#0B2447] text-white text-xs">
